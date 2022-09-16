084b3ea6bc8ab8c329f5f303c2ed5f7d066dc26c
Jesus Mezquiti <jmgcode@gmail.com>
2022-09-16 14:38:20 -0500

    refactor: replace lyrics query

--- src/Lyrics/Lyrics.tsx
+++ src/Lyrics/Lyrics.tsx
@@ -1,58 +1,24 @@
 import "./Lyrics.css";
-
-import { useEffect, useState } from "react";
-
-import { ITrack } from "../Tracks/TrackItem";
 import Loading from "../Loading";
-import axios from "axios";
+import { useAppSelector } from "../app/hooks";
+import { useGetLyricsQuery } from "../features/api/serverAPI/serverSlice";
 
-interface ILyrics {
-  playingTrack: ITrack | undefined;
-  show: boolean;
-}
-
-//TODO: Mover request fuera del componente para que no se vuelva a pedir en caso de ser
-//oculto con conditional render, si no se tendira que pasar siempre un prop que le diga
-//cuando renderear dentro del mismo componente
-const Lyrics: React.FC<ILyrics> = ({ playingTrack, show }) => {
-  const [lyrics, setLyrics] = useState("");
-  const [isLoading, setIsLoding] = useState(false);
-  useEffect(() => {
-    if (!playingTrack) return;
-    setIsLoding(true);
-    setTimeout(() => {
-      axios
-        .get("http://localhost:3001/lyrics", {
-          params: {
-            track: playingTrack.title,
-            artist: playingTrack.artist,
-          },
-        })
-        .then((res) => {
-          setLyrics(res.data.lyrics);
-          setIsLoding(false);
-        })
-        .catch(() => {
-          setLyrics("Server Error");
-        });
-    }, 1500);
-  }, [playingTrack]);
+const Lyrics = () => {
+  const playingTrack = useAppSelector((state) => state.dashboard.currTrack);
+  const { data, isLoading, isError } = useGetLyricsQuery(
+    { artist: playingTrack.artist, title: playingTrack.title },
+    { skip: !playingTrack.title || !playingTrack.artist }
+  );
 
   return (
-    <>
-      {show ? (
-        <Loading isLoading={isLoading}>
-          <div
-            className="text-center lyrics scrollable"
-            style={{ whiteSpace: "pre" }}
-          >
-            {lyrics}
-          </div>
-        </Loading>
-      ) : (
-        ""
-      )}
-    </>
+    <Loading isLoading={isLoading}>
+      <div
+        className="text-center lyrics scrollable"
+        style={{ whiteSpace: "pre" }}
+      >
+        {isError ? "Server Error" : data?.lyrics}
+      </div>
+    </Loading>
   );
 };
 
