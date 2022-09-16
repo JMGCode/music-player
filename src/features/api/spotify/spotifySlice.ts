57824bcccfe678e6bafdb7d33e3e4decd41fde90
Jesus Mezquiti <jmgcode@gmail.com>
2022-09-15 21:14:53 -0500

    feat: add authorization headers and transform responses

--- src/features/api/spotify/spotifySlice.ts
+++ src/features/api/spotify/spotifySlice.ts
@@ -1,23 +1,82 @@
-import { IAPIMe, IAPIPlaylist, IAPISearch } from "./interfaces";
+import { IMe, IPlaylists, ISearch, ISearchItem } from "./interfaces";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { IPlaylist } from "./interfaces/playlist";
+import { ITrack } from "../../../Tracks/TrackItem";
+import { RootState } from "../../../app/store";
+import { getSmallestImage } from "../../../helpers";
 
 // Define a service using a base URL and expected endpoints
 export const spotifyApiSlice = createApi({
   reducerPath: "spotifyApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://api.spotify.com/v1/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.spotify.com/v1/",
+    prepareHeaders: (headers, { getState }) => {
+      const state = getState() as RootState;
+      const token = state.auth.accessToken;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
-    getMe: builder.query<IAPIMe, void>({
+    getMe: builder.query<IMe, void>({
       query: () => `me`,
     }),
-    getPlaylists: builder.query<IAPIPlaylist, void>({
+    getPlaylists: builder.query<
+      { name: string; trackUri: string; id: any }[],
+      void
+    >({
       query: () => "me/playlists",
+      transformResponse: (response: IPlaylists) => {
+        return response.items.map((item) => {
+          return { name: item.name, trackUri: item.tracks.href, id: item.id };
+        });
+      },
     }),
-    searchByTrack: builder.query<IAPISearch, string>({
+    getPlaylistTracks: builder.query<ITrack[], string>({
+      query: (id) => `https://api.spotify.com/v1/playlists/${id}`,
+      transformResponse: (response: IPlaylist) => {
+        const items = response.tracks.items;
+        const filterItems = items.filter((item) => item.track !== null);
+
+        const trackItems = filterItems.map((item: any) => {
+          const { track } = item;
+          const smallestImage = getSmallestImage(track.album.images);
+
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestImage.url,
+          };
+        });
+        return trackItems;
+      },
+    }),
+    searchByTrack: builder.query<ITrack[], string>({
       query: (trackName = "") => `search/?type=track&q=${trackName}`,
+      transformResponse: (response: ISearch) => {
+        return response.tracks.items.map((item) => {
+          const smallestImage = getSmallestImage(item.album.images);
+          return {
+            artist: item.artists[0].name,
+            title: item.name,
+            uri: item.uri,
+            albumUrl: smallestImage.url,
+          };
+        });
+      },
     }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetMeQuery, useGetPlaylistsQuery } = spotifyApiSlice;
+export const {
+  useGetMeQuery,
+  useGetPlaylistsQuery,
+  useSearchByTrackQuery,
+  useGetPlaylistTracksQuery,
+} = spotifyApiSlice;
