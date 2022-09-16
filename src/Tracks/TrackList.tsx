b1e8fccffa030e8cbcacb42770406c56d689d75d
Jesus Mezquiti <jmgcode@gmail.com>
2022-09-16 14:40:02 -0500

    refactor : save playingTrack in store

--- src/Tracks/TrackList.tsx
+++ src/Tracks/TrackList.tsx
@@ -1,10 +1,13 @@
 import TrackItem, { ITrack } from "./TrackItem";
+import { useAppDispatch } from "../app/hooks";
+import { setPlayingTrack } from "../features/dashboard/dashboardSlice";
 interface ITracks {
   tracks: ITrack[] | undefined;
-  onChoose: Function;
 }
 
-const TrackList: React.FC<ITracks> = ({ tracks, onChoose }) => {
+const TrackList: React.FC<ITracks> = ({ tracks }) => {
+  const dispatch = useAppDispatch();
+
   return (
     <div className="scrollable">
       <div className="flex-grow-1 my-2">
@@ -13,7 +16,9 @@ const TrackList: React.FC<ITracks> = ({ tracks, onChoose }) => {
             <TrackItem
               track={track}
               key={track.uri + index}
-              chooseTrack={onChoose}
+              chooseTrack={(value: ITrack) => {
+                dispatch(setPlayingTrack(value));
+              }}
             />
           );
         })}
