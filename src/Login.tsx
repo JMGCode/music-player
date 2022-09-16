5468b175137adfbe17d9c7f39e2f069de6840061
Jesus Mezquiti <jmgcode@gmail.com>
2022-09-15 21:10:45 -0500

    feat: change authorize redirect url to localhost:3000/login

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,10 @@
 import { Container } from "react-bootstrap";
 
 const authUrl =
-  "https://accounts.spotify.com/authorize?client_id=fdbb32b746414133adaa41a22ace8ba5&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=fdbb32b746414133adaa41a22ace8ba5&response_type=code&redirect_uri=http://localhost:3000/login&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 const Login = () => {
+  //TODO: Remove Bootstrap
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
