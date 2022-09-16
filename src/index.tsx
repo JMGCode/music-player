fc29332ce61d38a66ad6f2eaf4ed1b99e9e75b3b
Jesus Mezquiti <jmgcode@gmail.com>
2022-09-15 21:16:53 -0500

    feat: Add persistor and router

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,50 @@
-import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import {
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+} from "react-router-dom";
+import { persistor, store } from "./app/store";
+
+import Dashboard from "./Dashboard";
+import ErrorPage from "./error-page";
+import OAuth from "./OAuth";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+
+    errorElement: <ErrorPage />,
+    loader: () => {
+      const token = store.getState().auth.accessToken;
+      if (token === "") {
+        return redirect("/login");
+      }
+    },
+  },
+  {
+    path: "/login",
+    element: <OAuth />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<p>loading ....</p>} persistor={persistor}>
+      <RouterProvider router={router} />
+    </PersistGate>
   </Provider>
   // </React.StrictMode>
 );
