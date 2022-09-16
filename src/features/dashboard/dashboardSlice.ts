335e848dbe5cd5765263ca6b13519ad356c57c61
Jesus Mezquiti <jmgcode@gmail.com>
2022-09-16 14:33:29 -0500

    feat: add playing track to store in dashboardSlice

---
+++ src/features/dashboard/dashboardSlice.ts
@@ -0,0 +1,29 @@
+//Payload is a typescript type that represents the contents of one given action object
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { ITrack } from "../../Tracks/TrackItem";
+
+interface IDashboard {
+  currTrack: ITrack;
+}
+
+const initialState: IDashboard = {
+  currTrack: {
+    artist: "",
+    albumUrl: "",
+    title: "",
+    uri: "",
+  },
+};
+
+const dashboardSlice = createSlice({
+  name: "dashboard",
+  initialState,
+  reducers: {
+    setPlayingTrack(state, action: PayloadAction<ITrack>) {
+      state.currTrack = action.payload;
+    },
+  },
+});
+
+export const { setPlayingTrack } = dashboardSlice.actions;
+export default dashboardSlice.reducer;
