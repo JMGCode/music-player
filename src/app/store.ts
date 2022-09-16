c2d9fecde59d7c2282acd3086d6147e6b2a903b8
Jesus Mezquiti <jmgcode@gmail.com>
2022-09-15 21:16:05 -0500

    feat: Persist auth data on local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,46 @@
+import {} from "../features/api/serverAPI/serverSlice";
+
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+
+import authReducer from "../features/auth/authSlice";
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
+import { serverApiSlice } from "../features/api/serverAPI/serverSlice";
 import { spotifyApiSlice } from "../features/api/spotify/spotifySlice";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "main-root",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    auth: persistedAuthReducer,
     [spotifyApiSlice.reducerPath]: spotifyApiSlice.reducer,
+    [serverApiSlice.reducerPath]: serverApiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(spotifyApiSlice.middleware);
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+      .concat(spotifyApiSlice.middleware)
+      .concat(serverApiSlice.middleware);
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+export const persistor = persistStore(store);
