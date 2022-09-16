563706d18e535a836124272e1a0840a3fab71836
Jesus Mezquiti <jmgcode@gmail.com>
2022-09-15 21:18:13 -0500

    feat: wrapper for login

---
+++ src/OAuth.tsx
@@ -0,0 +1,43 @@
+/**
+ * Spotify redirects to this page
+ * and with the code provided gets
+ * credentials token and refreshToken
+ * and dispatch them to store
+ */
+
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+
+import Login from "./Login";
+import { setCredentials } from "./features/auth/authSlice";
+import { useEffect } from "react";
+import { useLoginMutation } from "./features/api/serverAPI/serverSlice";
+import { useNavigate } from "react-router-dom";
+
+const OAuth = () => {
+  const [loginMutation] = useLoginMutation();
+  const { accessToken } = useAppSelector((state) => state.auth);
+
+  const dispatch = useAppDispatch();
+  let navigate = useNavigate();
+
+  const code = new URLSearchParams(window.location.search).get("code");
+
+  useEffect(() => {
+    async function login() {
+      if (accessToken !== "") navigate("/");
+      if (!code) return;
+      try {
+        const payload = await loginMutation(code).unwrap();
+        dispatch(setCredentials(payload));
+        navigate("/");
+      } catch (error) {
+        console.error("rejected", error);
+      }
+    }
+    login();
+  }, [code]);
+
+  return <Login />;
+};
+
+export default OAuth;
