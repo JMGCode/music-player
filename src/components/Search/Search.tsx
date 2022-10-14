204460d9e65c5a04d3adb1646452fcb3196f6713
Jesus Mezquiti <jmgcode@gmail.com>
2022-10-13 20:38:02 -0500

    refactor : Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,24 +1,55 @@
 import "./Search.css";
 
 import { Button, Form } from "react-bootstrap";
+import { CrossIcon, SearchIcon } from "../Icons";
 import React, { useState } from "react";
 
 interface ISearch {
   onChange: Function;
+  setSearch: Function;
+  search: string;
 }
 
 //TODO: Mover estilo del boton a un archivo css
 //TODO: Agregar funcion de log out
 //TODO: Mover boton log out a un componente separado de Search
-const Search: React.FC<ISearch> = ({ onChange }) => {
-  const [search, setSearch] = useState("");
+const Search: React.FC<ISearch> = ({ onChange, setSearch, search }) => {
+  // const [search, setSearch] = useState("");
 
   return (
-    <div className="search-bar">
-      <Form.Control
-        type="search"
-        placeholder="Search Songs/Artist"
+    <div
+      style={{
+        width: "40vw",
+        height: "40px",
+        backgroundColor: "white",
+        borderRadius: "20px",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          margin: 0,
+          height: "100%",
+          paddingLeft: "20px",
+        }}
+      >
+        <SearchIcon color="black" size="20" />
+      </div>
+      <input
         value={search}
+        placeholder="What do you want to listen to?"
+        style={{
+          outline: "none",
+          outlineWidth: "0",
+          marginLeft: "10px",
+          height: "100%",
+          width: "calc(100% - 80px)",
+          // width: "80%",
+          border: "none",
+        }}
         onChange={(e) => {
           const { value } = e.target;
           //debounce on change
@@ -26,7 +57,28 @@ const Search: React.FC<ISearch> = ({ onChange }) => {
           //keep without debounce to show new input on search bar
           setSearch(value);
         }}
-      />
+      ></input>
+      {search !== "" && (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            margin: 0,
+            height: "100%",
+            paddingRight: "20px",
+            paddingLeft: "12px",
+          }}
+        >
+          <CrossIcon
+            color="black"
+            size="18"
+            onClick={() => {
+              onChange("");
+              setSearch("");
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
