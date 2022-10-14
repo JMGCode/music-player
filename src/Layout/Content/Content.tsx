cd983ca6573762e31265680603b3d1f82e7229ae
Jesus Mezquiti <jmgcode@gmail.com>
2022-10-13 20:45:10 -0500

    feat: Make content scrollable

--- src/Layout/Content/Content.tsx
+++ src/Layout/Content/Content.tsx
@@ -3,7 +3,7 @@ import "./Content.css";
 import { PropsWithChildren } from "react";
 
 const Content: React.FC<PropsWithChildren<unknown>> = ({ children }) => {
-  return <div className="content">{children}</div>;
+  return <div className="content scrollable">{children}</div>;
 };
 
 export default Content;
