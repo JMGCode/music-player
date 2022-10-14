5a52a728ebd7b580b98d3f8cffb1849205a86c13
Jesus Mezquiti <jmgcode@gmail.com>
2022-10-13 20:48:45 -0500

    feat: temp library and home pages

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../Layout/Header";
+
+const HomePage = () => {
+  return (
+    <div className="page-padding">
+      <Header></Header>
+      <h1>Home!!!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/Library/LibraryPage.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../Layout/Header";
+
+const LibraryPage = () => {
+  return (
+    <div className="page-padding">
+      <Header></Header>
+      <h1>Your Library</h1>
+    </div>
+  );
+};
+
+export default LibraryPage;
