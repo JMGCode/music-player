e9fe67b972f5da534c7856849edd3de2a7f56b3f
Jesus Mezquiti <jmgcode@gmail.com>
2022-10-23 02:23:41 -0500

    feat: folder with notications content

---
+++ src/Notifications/InfoNoPremium.tsx
@@ -0,0 +1,17 @@
+const InfoNoPremium = () => {
+  return (
+    <div
+      style={{
+        flex: "1",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <div>
+        Web Playback SDK is only available with a premium spotify account
+      </div>
+    </div>
+  );
+};
+export default InfoNoPremium;
---
+++ src/Notifications/index.ts
@@ -0,0 +1,2 @@
+export { default as InfoNoPremium } from "./InfoNoPremium";
+export { default as Error404NoPremium } from "./Error403NoPremium";
