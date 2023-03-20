a91e47cfd194bfacc6d97c0054e5e057f97584a1
Jesus Mezquiti <jmgcode@gmail.com>
2023-03-20 14:53:01 -0600

    feat: update header styles

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,16 +1,21 @@
 import "./Header.css";
 
-import { FC, PropsWithChildren, useEffect, useState } from "react";
 import {
   DropDownArrow,
   LeftCircleArrowIcon,
   RightCircleArrowIcon,
 } from "../../components/Icons";
+import { FC, PropsWithChildren, useEffect, useRef, useState } from "react";
+import {
+  getRandColorFromStr,
+  getRandColorFromStrs,
+} from "../../helpers/getRandColorFromStr";
 
-import { useNavigate } from "react-router-dom";
-import { useGetMeQuery } from "../../features/api/spotify/me";
-import { useAppDispatch } from "../../app/hooks";
 import { clearCredentials } from "../../features/auth/authSlice";
+import { useAppDispatch } from "../../app/hooks";
+import { useGetMeQuery } from "../../features/api/spotify/me";
+import { useNavigate } from "react-router-dom";
+import useOutsideAlerter from "../../hooks/useOutsideAlerter";
 
 interface IProps {
   styles?: any;
@@ -52,7 +57,14 @@ const Header: FC<PropsWithChildren<IProps>> = ({ children, styles }) => {
   const { data: user } = useGetMeQuery();
   const [isClosing, setIsClosing] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
+  const [userColor, setUserColor] = useState({ rgb: "rgb(0,0,0)", hsl: "" });
   const dispatch = useAppDispatch();
+  const userMenuRef = useRef(null);
+  useOutsideAlerter(userMenuRef, () => {
+    if (!isVisible) {
+      setIsClosing(true);
+    }
+  });
 
   const extraMenuItems = [
     {
@@ -75,6 +87,15 @@ const Header: FC<PropsWithChildren<IProps>> = ({ children, styles }) => {
     }
   }, [isClosing]);
 
+  useEffect(() => {
+    if (!user) return;
+    const { red, green, blue, h } = getRandColorFromStr(user?.display_name);
+    // getRandColorFromStrs(user?.display_name, { min: 359, max: 2 });
+    setUserColor({
+      rgb: `rgb(${red},${green},${blue})`,
+      hsl: `hsl(${h},100%,50%)`,
+    });
+  }, [user]);
   return (
     <div className="header-container" style={{ ...container }}>
       <div className="header-navigation-container">
@@ -93,8 +114,39 @@ const Header: FC<PropsWithChildren<IProps>> = ({ children, styles }) => {
           }
         }}
       >
-        <img className="user-img" src={user?.images[0].url} alt="profile_img" />
-        <span className="user-name">{user?.display_name}</span>
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+            overflow: "hidden",
+          }}
+        >
+          {user?.images.length > 0 ? (
+            <img
+              className="user-img"
+              src={user?.images[0].url}
+              alt="profile_img"
+            />
+          ) : (
+            <p
+              style={{
+                backgroundColor: userColor.hsl,
+                borderRadius: "50%",
+                height: "80%",
+                aspectRatio: "1",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                fontWeight: "bold",
+              }}
+            >
+              {user?.display_name[0]}
+            </p>
+          )}
+          <span className="user-name">{user?.display_name}</span>
+        </div>
         <div
           style={{
             height: "100%",
@@ -106,6 +158,7 @@ const Header: FC<PropsWithChildren<IProps>> = ({ children, styles }) => {
           <DropDownArrow size="20" />
         </div>
         <div
+          ref={userMenuRef}
           className="header-user-menu"
           style={{
             animation: `${
