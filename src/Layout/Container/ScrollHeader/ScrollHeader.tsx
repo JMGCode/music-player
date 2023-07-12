5e2a1e487a60daee0a96b0f398eee9b1c79156df
Jesus Mezquiti <jmgcode@gmail.com>
2023-07-12 00:35:44 -0600

    feat: add new properties to ScrollHeader for the playbutton functionality

--- src/Layout/Container/ScrollHeader/ScrollHeader.tsx
+++ src/Layout/Container/ScrollHeader/ScrollHeader.tsx
@@ -8,11 +8,21 @@ import { getRandColorFromStr } from "../../../helpers/getRandColorFromStr";
 import useObservableIntersection from "../../../hooks/useObservableIntersection";
 
 interface Props {
+  onPlay?: () => void;
+  isPlaying?: boolean;
   title?: string;
   height?: number;
   children?: React.ReactNode;
+  onAlphaChange?: (value: number) => void;
 }
-const ScrollHeader: FC<Props> = ({ title, height = 324, children }) => {
+const ScrollHeader: FC<Props> = ({
+  title,
+  height = 324,
+  children,
+  onPlay,
+  isPlaying = false,
+  onAlphaChange = () => {},
+}) => {
   const [headerTitleVisible, setHeaderTitleVisible] = useState(false);
   const [headerAlpha, setHeaderAlpha] = useState(0);
 
@@ -21,10 +31,13 @@ const ScrollHeader: FC<Props> = ({ title, height = 324, children }) => {
     // console.log("header title visible: ", headerTitleVisible);
     if (ratio <= 0.4) {
       setHeaderAlpha(1);
+      onAlphaChange(1);
     } else if (ratio > 0.8) {
       setHeaderAlpha(0);
+      onAlphaChange(0);
     } else {
       setHeaderAlpha(1 - ratio);
+      onAlphaChange(1 - ratio);
     }
 
     if (ratio <= 0.4 && !headerTitleVisible) {
@@ -77,7 +90,12 @@ const ScrollHeader: FC<Props> = ({ title, height = 324, children }) => {
           }`}
         >
           {headerTitleVisible && title && (
-            <PlayerButton width="35px" height="35px" state="paused" />
+            <PlayerButton
+              width="45px"
+              height="45px"
+              state={!isPlaying ? "paused" : "playing"}
+              onClick={onPlay}
+            />
           )}
 
           <span>{title}</span>
@@ -99,6 +117,11 @@ const ScrollHeader: FC<Props> = ({ title, height = 324, children }) => {
 
 export default ScrollHeader;
 
-export const ScrollHeaderContent: FC<{ children: React.ReactNode }> = ({
-  children,
-}) => <div style={{ position: "relative", padding: "0 30px" }}>{children}</div>;
+export const ScrollHeaderContent: FC<{
+  children: React.ReactNode;
+  noPadding?: boolean;
+}> = ({ children, noPadding }) => (
+  <div style={{ position: "relative", padding: noPadding ? "0" : "0 30px" }}>
+    {children}
+  </div>
+);
