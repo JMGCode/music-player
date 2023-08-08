1f6cba776ca44e5be549108d6c05e61826442aa2
Jesus Mezquiti <jmgcode@gmail.com>
2023-08-08 13:28:27 -0600

    feat: add useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,61 @@
+import { useEffect, useReducer, useState } from "react";
+
+const reducer = (state: any, { type, responseJSON, error }: any) => {
+  switch (type) {
+    case "skip":
+      return { ...state, isLoading: false };
+    case "loading":
+      return { ...state, isLoading: true };
+    case "success":
+      return { responseJSON, isLoading: false, error: null };
+    case "error":
+      return { responseJSON: null, isLoading: false, error };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+};
+
+const useFetch = (url: string, flags?: { skip?: boolean }) => {
+  // const [responseJson, setResponseJson] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+
+  const [state, dispatch] = useReducer(reducer, {
+    responseJSON: null,
+    isLoading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    let shouldCancel = false;
+
+    const callFetch = async () => {
+      if (flags?.skip) {
+        dispatch({ type: "skip" });
+        return;
+      }
+      dispatch({ type: "loading" });
+      try {
+        const response = await fetch(url);
+        const responseJSON = await response.json();
+
+        if (shouldCancel) return;
+        dispatch({ type: "success", responseJSON });
+      } catch (error: any) {
+        if (shouldCancel) return;
+        dispatch({ type: "error", error });
+      }
+    };
+
+    callFetch();
+
+    return () => {
+      shouldCancel = true;
+    };
+  }, [url]);
+
+  return state;
+};
+
+export default useFetch;
