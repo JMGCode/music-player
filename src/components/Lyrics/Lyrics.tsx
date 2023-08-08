f3d80732dd21524177ecd05691da1f28f4e1896e
Jesus Mezquiti <jmgcode@gmail.com>
2023-08-08 13:29:35 -0600

    refactor: change lyrics backend endpoint for a local fetch to a public api

--- src/components/Lyrics/Lyrics.tsx
+++ src/components/Lyrics/Lyrics.tsx
@@ -1,25 +1,41 @@
 import "./Lyrics.css";
+
 import Loading from "../Loading/Loading";
 import { useAppSelector } from "../../app/hooks";
-import { useGetLyricsQuery } from "../../features/api/serverAPI";
+import useFetch from "../../hooks/useFetch";
 
 const Lyrics = () => {
   const playingTrack = useAppSelector((state) => state.dashboard.currTrack);
-  const { data, isLoading, isError } = useGetLyricsQuery(
-    {
-      artist: playingTrack?.artists[0].name || "",
-      title: playingTrack?.name || "",
-    },
-    { skip: !playingTrack?.name || !playingTrack?.artists }
+
+  const { responseJSON, isLoading, error } = useFetch(
+    `https://spotify-lyric-api.herokuapp.com/?trackid=${playingTrack?.id}`,
+    { skip: !playingTrack }
   );
 
   return (
     <Loading isLoading={isLoading}>
-      <div
-        className="text-center lyrics scrollable"
-        style={{ whiteSpace: "pre" }}
-      >
-        {isError ? "Server Error" : data?.lyrics}
+      <div className="text-center lyrics ">
+        {error || responseJSON?.error ? (
+          "Server Error"
+        ) : (
+          <div>
+            {responseJSON?.lines.map((line: any, index: number) => (
+              <p
+                key={index}
+                style={{
+                  borderRadius: "20px",
+                  padding: "10px",
+                  // backgroundColor:
+                  //   index === counter && line.words !== ""
+                  //     ? "rgba(255,255,255,0.1)"
+                  //     : "",
+                }}
+              >
+                {line.words}
+              </p>
+            ))}
+          </div>
+        )}
       </div>
     </Loading>
   );
